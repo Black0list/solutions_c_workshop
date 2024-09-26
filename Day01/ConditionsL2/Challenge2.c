0cb2c9c0d1ce8335366b7bd6645f7f57cee732e7
ABDELKEBIR HADOUI <abdelkebir123123@gmail.com>
2024-09-26 14:52:29 +0200

    fix code Bug +30%

--- Day01/ConditionsL2/Challenge2.c
+++ Day01/ConditionsL2/Challenge2.c
@@ -2,10 +2,7 @@
 
 int main() {
     int age, voiture, nbr_accidents;
-    float prime_de_base;
-
-    printf("Entrer montant initial de la prime : ");
-    scanf("%f", &prime_de_base);
+    float const prime_de_base = 3000;
 
     printf("Âge du conducteur (en années) : ");
     scanf("%d", &age);
@@ -16,34 +13,33 @@ int main() {
     printf("Nombre d'accidents au cours des 5 dernières années : ");
     scanf("%d", &nbr_accidents);
 
-    float prime = prime_de_base; // Initialiser la prime ajustée à la prime de base
-
-    // Calcul de la prime selon l'âge
+    float prime = prime_de_base; 
     if (age < 25) {
         prime *= 1.5;
     } else if (age > 65) {
         prime *= 1.2;
     }
-    // Pour l'âge de 25 à 65, la prime reste inchangée.
 
-    // Ajustement de la prime selon le type de voiture
-    if (voiture == 1) {
-        prime *= 2;
-    } else if (voiture == 2) {
-        prime *= 1.2;
-    } else if (voiture == 3) {
-        prime *= 1.1;
-    } else {
-        printf("Type de voiture invalide.\n");
-        return 1;
+    switch(voiture){
+
+        case 1: prime *=2;
+        break;
+
+        case 2: prime *=1.2;
+        break;
+
+        case 3: prime *=1.1;
+        break;
+
+        default: printf("Type de voiture invalide.\n");
+        break;
     }
 
-    // Ajustement de la prime selon le nombre d'accidents
     if (nbr_accidents > 1) {
-        prime *= 1.3; // Ajout de 30%
+        prime += prime * 0.3; 
     }
 
-    // Affichage de la prime finale
+
     printf("La prime d'assurance est : %.2f\n", prime);
 
     return 0;
