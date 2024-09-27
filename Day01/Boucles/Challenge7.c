2c15d41be1654fea68bf0723edfb3d740a687e1c
ABDELKEBIR HADOUI <abdelkebir123123@gmail.com>
2024-09-27 15:16:51 +0200

    Error Posting Challenge7

--- Day01/Boucles/Challenge7.c
+++ Day01/Boucles/Challenge7.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
- 
-int main()
-{
-  int n, nbr1 = 0, nbr2 = 1, suivant, i;
- 
-  printf("Entrez le nombre de termes\n");
-  scanf("%d", &n);
- 
-  printf("Les %d premiers termes de la série de Fibonacci sont:\n", n);
- 
-  for (i = 0; i < n; i++)
-  {
-    if (i <= 1)
-      suivant = i;
-    else
-    {
-      suivant = nbr1 + nbr2;
-      nbr1 = nbr2;
-      nbr2 = suivant;
-    }
-    printf("%d\n", suivant);
-  }
- 
-  return 0;
+
+int main(){
+
+int nbr, inverse;
+
+printf("entrer un Nombre :");
+scanf("%d",&inverse);
+
+nbr = inverse;
+inverse = 0;
+
+while(nbr != 0){
+  inverse = inverse * 10;
+  inverse = inverse + nbr%10;
+  nbr = nbr/10;
+}
+
+printf("inverse est : %d", inverse);
+
 }
